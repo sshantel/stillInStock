c256c4a949db1a23b123b058d615da0520dac87f
Chantel Yip <chantelyip422@gmail.com>
2021-01-28 01:25:01 -0800

    added basicfinancialurl fetch

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const FINNHUB_API_KEY = process.env.FINNHUB_API_KEY;
-console.log(FINNHUB_API_KEY);
 const fetch = require("node-fetch");
 
 const readline = require("readline").createInterface({
@@ -12,9 +11,9 @@ const searchFinnhub = (stocktickerSym) => {
   readline.question("What is your stocktickerSymbol? ", (stocktickerSym) => {
     console.log(`Fetching results for ${stocktickerSym}...`);
     readline.close;
-    const url = `https://finnhub.io/api/v1/search?q=${stocktickerSym}&token=${FINNHUB_API_KEY}`;
+    const webhookUrl = `https://finnhub.io/api/v1/search?q=${stocktickerSym}&token=${FINNHUB_API_KEY}`;
 
-    fetch(url)
+    fetch(webhookUrl)
       .then((response) => {
         if (response.status === 200) {
           console.log("yay!");
@@ -27,14 +26,37 @@ const searchFinnhub = (stocktickerSym) => {
         console.log(
           `we found ${data.count} results! Please confirm which one it is`
         );
-        readline.question("Which symbol is it? ", (symbol, description) => {});
-        console.log(
-          data.result.map((r) => ({
-            description: r.description,
-            symbol: r.symbol,
-          }))
-        );
+        data.result.forEach((r, i) => {
+          const selection = i + 1;
+          const { description, symbol } = r;
+          console.log(
+            `${selection} description: ${description} symbol: ${symbol}`
+          );
+        });
+        readline.question("Which # is it? ", (stocktickerSymbol) => {
+          const basicFinancialsUrl = `https://finnhub.io/api/v1//stock/metric?symbol=${stocktickerSymbol}&metric=all&token=${FINNHUB_API_KEY}`;
+          fetch(basicFinancialsUrl)
+            .then((response) => {
+              if (response.status === 200) {
+                console.log("yay!");
+              } else {
+                throw Error(response.statusText);
+              }
+              return response.json();
+            })
+            .then((data) => {});
+          data.result.forEach((r, i) => {
+            const { annual } = r;
+          });
+        });
       });
   });
 };
 searchFinnhub();
+
+// console.log(
+//   data.result.map((r) => ({
+//     description: r.description,
+//     symbol: r.symbol,
+//   }))
+// );
